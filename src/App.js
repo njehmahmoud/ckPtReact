20d2337ee551a6347474ce25d57541833ad6ed5f
njehmahmoud <njehmahmoud95@gmail.com>
2021-05-14 20:21:05 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <head>
+  <meta name="viewport" content="width=device-width, initial-scale=1"/>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"/>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+    <div className="container"> Hello world!!!!!</div>
+    <button type="button" class="btn btn-success">Success</button>
+
+    </div>
+  );
+}
+
+export default App;
