255e92d3dc7b854ec0a49b419bd8ffe13b52652c
njehmahmoud <njehmahmoud95@gmail.com>
2021-05-20 15:12:33 +0100

    final ckPtJsx

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import'./style.css';
+import imageInSrc from './imageInSrc.jpg'
 
 function App() {
   return (
     <div className="App">
-      <head>
-  <meta name="viewport" content="width=device-width, initial-scale=1"/>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"/>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-</head>
-    <div className="container"> Hello world!!!!!</div>
-    <button type="button" class="btn btn-success">Success</button>
+      <div style={{border:'solid 1px black', maxWidth:'100vw'}}>
+
+        <h1 className="title red">Your name here</h1>
+
+
+
+<img src={imageInSrc} alt ='myImage' />
+
+<br />
+
+<img src="/imageInPublic.jpg" />
+
+</div>
+
+{ <video width="320" height="240" controls>
+
+<source src="myVideo.mp4" type="video/mp4" />
+
+</video> }
 
     </div>
   );
